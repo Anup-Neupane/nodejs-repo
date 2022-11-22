e8da5d85216bc3a98979bd78076298df52a48f1f
Anup-Neupane <100012091+Anup-Neupane@users.noreply.github.com>
2022-11-22 11:12:04 +0545

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,4 +3,4 @@
 echo 'run application_start.sh: ' >> /home/ec2-user/nodjs/deploy.log
 
 echo 'sudo pm2 restart nodejs-express-app' >> /home/ec2-user/nodjs/deploy.log
-sudo pm2 restart nodejs-express-appp >> /home/ec2-user/nodjs/deploy.log
+sudo pm2 restart nodejs-express-app >> /home/ec2-user/nodjs/deploy.log
