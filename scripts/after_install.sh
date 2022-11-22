375f809da741369ad2ccbcac21792d2ad9fc72cf
Anup-Neupane <100012091+Anup-Neupane@users.noreply.github.com>
2022-11-22 09:44:29 +0545

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo 'run after_install.sh: ' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
+echo 'run after_install.sh: ' >> /home/ec2-user/nodjs/deploy.log
 
-echo 'cd /home/ec2-user/nodejs-server-cicd' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
-cd /home/ec2-user/nodejs-aws-codedeploy-pipeline >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
+echo 'cd /home/ec2-user/nodejs-server-cicd' >> /home/ec2-user/nodjs/deploy.log
+cd /home/ec2-user/nodjs >> /home/ec2-user/nodjs/deploy.log
 
-echo 'npm install' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log 
-npm install >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
+echo 'sudo npm install' >> /home/ec2-user/nodjs/deploy.log 
+sudo npm install >> /home/ec2-user/nodjs/deploy.log
